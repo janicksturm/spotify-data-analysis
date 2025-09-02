42279958b72273425f16b158693b1b7acc3fd388
Janick Sturm <sturmjanick@gmail.com>
2025-09-02 22:57:10 +0200

    refactor: move data loading logic to dataLoader.py for better organization

--- scripts/main.py
+++ scripts/main.py
@@ -1,17 +1,6 @@
-from extractData import read_json_file, get_Track_Name, calc_track_duration
+from dataLoader import FULL_DATA
+from extractData import get_Track_Name, calc_track_duration
 from convertTime import calc_ms_in_hours, calc_hours_in_days
-
-
-DATA_1619 = read_json_file('../data/spotifyStreamData/Streaming_History_Audio_2016-2019_0.json')
-DATA_1922 = read_json_file('../data/spotifyStreamData/Streaming_History_Audio_2019-2022_1.json')
-DATA_2223 = read_json_file('../data/spotifyStreamData/Streaming_History_Audio_2022-2023_2.json')
-DATA_23 = read_json_file('../data/spotifyStreamData/Streaming_History_Audio_2023_3.json')
-DATA_2324 = read_json_file('../data/spotifyStreamData/Streaming_History_Audio_2023-2024_4.json')
-DATA_24 = read_json_file('../data/spotifyStreamData/Streaming_History_Audio_2024_5.json')
-DATA_2425 = read_json_file('../data/spotifyStreamData/Streaming_History_Audio_2024-2025_6.json')
-DATA25 = read_json_file('../data/spotifyStreamData/Streaming_History_Audio_2025_7.json')
-
-FULL_DATA = DATA_1619 + DATA_1922 + DATA_2223 + DATA_23 + DATA_2324 + DATA_24 + DATA_2425 + DATA25
 
 get_Track_Name(FULL_DATA)
 
